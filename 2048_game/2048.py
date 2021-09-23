5f7e3891431717e78fbc33367f3a77e0aff6f140
SlipShabby <sd.djaan@gmail.com>
2021-09-23 11:48:16 -0700

    solutions

--- 2048_game/2048.py
+++ 2048_game/2048.py
@@ -1,1 +1,14 @@
 # my implementation of 2048 game
+''''
+Steps:
+1 create 4*4 grid, with initially 2 cells filled with 2 random numbers.
+2 user pass cmds which cell to choose and where to move 
+    cmds: up, down, left, right, choose
+3 so cells values adds up, moved cell become empty
+4 2 random cells are filled with 2 random numbers
+5 the goal is to reach 2048 value
+    - goal reached: win!
+    - if goal is not reached and there is no 2 cells to be filled with 2 new cells: game over!
+
+'''
+
